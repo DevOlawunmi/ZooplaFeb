32aafb56b5d3de636b4fd51371398fd943770d25
Ola <olawunmi263@yahoo.com>
2020-02-13 03:43:22 +0000

    Test run and passed

---
+++ src/test/java/co/uk/zooplaa/stepDefinitions/ForSaleSearchSteps.java
@@ -0,0 +1,59 @@
+package co.uk.zooplaa.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ForSaleSearchSteps
+
+{
+    @Given("I navigate to Zoopla.co.uk")
+    public void i_navigate_to_Zoopla_co_uk() {
+
+    }
+
+    @Given("the To Buy tab is highlighted")
+    public void the_To_Buy_tab_is_highlighted() {
+
+    }
+
+    @When("I enter a {string} in the search bar")
+    public void i_enter_a_in_the_search_bar(String location) {
+
+    }
+
+    @When("I select {string} from the Min dropdown")
+    public void i_select_from_the_Min_dropdown(String minimum) {
+
+    }
+
+    @When("I select {string} from the Max dropdown")
+    public void i_select_from_the_Max_dropdown(String price) {
+
+    }
+
+    @When("I select {string} from the Property dropdown")
+    public void i_select_from_the_Property_dropdown(String property) {
+
+    }
+
+    @When("I select {string} from the rooms dropdown")
+    public void i_select_from_the_rooms_dropdown(String rooms) {
+
+    }
+
+    @When("I click on the Search button")
+    public void i_click_on_the_Search_button() {
+
+    }
+
+    @Then("all the property within the selected location and price range are displayed")
+    public void all_the_property_within_the_selected_location_and_price_range_are_displayed() {
+
+    }
+
+
+
+
+
+}
