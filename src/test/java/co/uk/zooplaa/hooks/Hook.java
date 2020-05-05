1b593d2c298a3085a7923bc1c474545eac77c4d6
Ola <olawunmi263@yahoo.com>
2020-05-05 04:15:18 +0100

    Hook updated

--- src/test/java/co/uk/zooplaa/hooks/Hook.java
+++ src/test/java/co/uk/zooplaa/hooks/Hook.java
@@ -3,19 +3,33 @@ package co.uk.zooplaa.hooks;
 import co.uk.zooplaa.common.Browser;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook extends Browser
 
+
 {
+    public Scenario scenario;
     public String defaultBrowser  = "Chrome";
     @Before
-    public void setUp()
-    {
+    public void setUp(Scenario scenario)
+    {this.scenario = scenario;
         launchBrowser(defaultBrowser);
     }
     @After
     public void tearDown()
-    {
+
+    {if(scenario.isFailed()){
+        try {
+            byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot,"screenshot.png","failurePage");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
         closeBrowser();
     }
 }
